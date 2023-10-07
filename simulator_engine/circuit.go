7ab7cca606aee190f024a02413789132741b4ac7
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2023-10-07 16:52:38 +0300

    Implement ApplyGate function

--- simulator_engine/circuit.go
+++ simulator_engine/circuit.go
@@ -1,5 +1,7 @@
 package simulatorengine
 
+import "slices"
+
 type QuantumGate struct {
 	Qubits             []int
 	BasisStatesActions map[string]map[string]complex128
@@ -29,35 +31,39 @@ func (g QuantumGate) ApplyGateContiguousQubits(input Qudit) Qudit {
 func (g QuantumGate) ApplyGate(input Qudit) Qudit {
 	output_amplitudes := make(map[string]complex128)
 	for basis_state, amplitude := range input.Amplitudes {
-		// Accumulate the contributions to the output state
-		// for the current input basis state
-		for contribution_basis_state, contribution_amplitude := range g.BasisStatesActions[basis_state] {
-			_, exists := output_amplitudes[contribution_basis_state]
+		partial_state := getStateOnQubitsSubset(basis_state, g.Qubits)
+		// Apply the gate on the specified qubits
+		partial_state_qudit := g.ApplyGateContiguousQubits(Qudit{N_qubits: len(g.Qubits), Amplitudes: map[string]complex128{partial_state: amplitude}})
+		// Iterate through the base states defined on the gate's qubits
+		for partial_basis_state, partial_amplitude := range partial_state_qudit.Amplitudes {
+			new_basis_state := ""
+			// Construct the states on all qubits, after the gate was applied
+			for i := range basis_state {
+				// The qubit state is either from the output of the gate, or the original basis state value
+				if slices.Contains(g.Qubits, i) {
+					index_in_partial_basis_state := slices.Index(g.Qubits, i)
+					new_basis_state += partial_basis_state[index_in_partial_basis_state : index_in_partial_basis_state+1]
+				} else {
+					new_basis_state += basis_state[i : i+1]
+				}
+			}
+			cr_amplitude, exists := output_amplitudes[new_basis_state]
 			if exists {
-				output_amplitudes[contribution_basis_state] += amplitude * contribution_amplitude
+				output_amplitudes[new_basis_state] = cr_amplitude + partial_amplitude
 			} else {
-				output_amplitudes[contribution_basis_state] = amplitude * contribution_amplitude
+				output_amplitudes[new_basis_state] = partial_amplitude
 			}
 		}
+
 	}
 	output := Qudit{N_qubits: input.N_qubits, Amplitudes: output_amplitudes}
 	return output
 }
 
-/*
-func getStateOnSpecifiedQubits(qubits_indices []int, full_state Qudit) Qudit {
-
-		for basis_state, amplitude := range full_state.Amplitudes {
-
-		}
-
-		return full_state
-	}
-*/
-
-func getStateOnQubitsSubset(input_state string, qubit_indices []int) string {
+// Get the state for a subset of qubits
+func getStateOnQubitsSubset(input_state string, qubits_indices []int) string {
 	partial_state := ""
-	for _, qubit_index := range qubit_indices {
+	for _, qubit_index := range qubits_indices {
 		partial_state += input_state[qubit_index : qubit_index+1]
 	}
 	return partial_state
