f8a74441c0c03685f8917f57e713f708f4b8b7cb
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2023-10-08 14:39:08 +0300

    Implement ApplyCircuit

--- simulator_engine/circuit.go
+++ simulator_engine/circuit.go
@@ -73,7 +73,11 @@ type QuantumCircuit struct {
 	Gates []QuantumGate
 }
 
-func ApplyCircuit(input Qudit) Qudit {
-	output := Qudit{input.N_qubits, input.Amplitudes}
-	return output
+// Method that simulates a quantum circuit
+func (qc QuantumCircuit) ApplyCircuit(input Qudit) Qudit {
+	qudit_after_gate := input
+	for _, gate := range qc.Gates {
+		qudit_after_gate = gate.ApplyGate(qudit_after_gate)
+	}
+	return qudit_after_gate
 }
