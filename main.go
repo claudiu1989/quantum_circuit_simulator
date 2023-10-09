4b3eb4f2533d7497214c4b66e235f21a745b676e
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2023-10-09 14:24:16 +0300

    Add function to parse the input qubit

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	simulatorengine "github.com/claudiu1989/quantum_circuit_simulator/simulator_engine"
-)
-
 func main() {
-	simulatorengine.ReadFile("./circuits_examples/circuit1.circuit")
+
 }
--- simulator_engine/parser.go
+++ simulator_engine/parser.go
@@ -4,9 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func ReadFile(file_path string) []string {
+const specialSplitString = "***"
+
+func readFile(file_path string) []string {
 	read_file, err := os.Open(file_path)
 
 	if err != nil {
@@ -23,4 +27,32 @@ func ReadFile(file_path string) []string {
 
 	read_file.Close()
 	return file_lines
+}
+
+func parseGetInputQudit(file_lines []string) Qudit {
+	input_qudit_string := file_lines[1]
+	var amplitudes map[string]complex128
+	var augumente_qudit_sb strings.Builder
+	for i, rune_qudit := range input_qudit_string {
+		if i < len(input_qudit_string)-1 && (input_qudit_string[i+1:i+2] == "+" || input_qudit_string[i+1:i+2] == "-") {
+			augumente_qudit_sb.WriteString(specialSplitString)
+		}
+		augumente_qudit_sb.WriteRune(rune_qudit)
+	}
+	var n_qubits int
+	amplitude_state_pairs := strings.Split(augumente_qudit_sb.String(), specialSplitString)
+	for _, cr_amplitude_state_pair := range amplitude_state_pairs {
+		amplitude_state_arr := strings.Split(cr_amplitude_state_pair, "|")
+		cr_amplitude, err := strconv.ParseComplex(amplitude_state_arr[0], 128)
+		cr_state := amplitude_state_arr[1][:len(amplitude_state_arr[1])-1]
+		n_qubits = len(cr_state)
+		amplitudes[cr_state] = cr_amplitude
+	}
+
+	return Qudit{N_qubits: n_qubits, Amplitudes: amplitudes}
+}
+
+func ParseFile(file_path string) {
+	file_lines := readFile(file_path)
+	fmt.Println(file_lines)
 }
