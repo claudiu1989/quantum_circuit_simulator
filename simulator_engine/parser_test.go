9330ee8dd28db54f09468cd69b232e9dfa3734f1
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2023-10-09 15:43:43 +0300

    Add more test cases for the parser

--- simulator_engine/parser_test.go
+++ simulator_engine/parser_test.go
@@ -4,16 +4,44 @@ import (
 	"testing"
 )
 
+type testCaseParseGetInputQudit struct {
+	FileLines          []string
+	ExpectedInputQudit Qudit
+}
+
+var testCasesParseGetInputQudit = []testCaseParseGetInputQudit{
+	{
+		[]string{"Input", "1|0>+0|1>"},
+		Qudit{N_qubits: 1, Amplitudes: map[string]complex128{"0": complex(1, 0), "1": complex(0, 0)}},
+	},
+	{
+		[]string{"Input", "0.707|0>+0.707|1>"},
+		Qudit{N_qubits: 1, Amplitudes: map[string]complex128{"0": complex(0.707, 0), "1": complex(0.707, 0)}},
+	},
+	{
+		[]string{"Input", "0.707i|0>+0.707|1>"},
+		Qudit{N_qubits: 1, Amplitudes: map[string]complex128{"0": complex(0, 0.707), "1": complex(0.707, 0)}},
+	},
+	{
+		[]string{"Input", "0.707i|0>+0.707i|1>"},
+		Qudit{N_qubits: 1, Amplitudes: map[string]complex128{"0": complex(0, 0.707), "1": complex(0, 0.707)}},
+	},
+	{
+		[]string{"Input", "0.707i|001>+0.707i|011>"},
+		Qudit{N_qubits: 3, Amplitudes: map[string]complex128{"001": complex(0, 0.707), "011": complex(0, 0.707)}},
+	},
+}
+
 func TestParseGetInputQudit(t *testing.T) {
-	file_lines := []string{"Input", "1|0>+0|1>"}
-	expected_input_qudit := Qudit{N_qubits: 1, Amplitudes: map[string]complex128{"0": complex(1, 0), "1": complex(0, 0)}}
-	input_qudit := parseGetInputQudit(file_lines)
-	if input_qudit.N_qubits != expected_input_qudit.N_qubits {
-		t.Errorf("The number of qubits is %d instead of %d", input_qudit.N_qubits, expected_input_qudit.N_qubits)
-	}
-	for input_state, input_amplitude := range input_qudit.Amplitudes {
-		if input_amplitude != expected_input_qudit.Amplitudes[input_state] {
-			t.Errorf("The amplitude for state %s is %g instead of %g", input_state, input_amplitude, expected_input_qudit.Amplitudes[input_state])
+	for _, test_case := range testCasesParseGetInputQudit {
+		input_qudit := parseGetInputQudit(test_case.FileLines)
+		if input_qudit.N_qubits != test_case.ExpectedInputQudit.N_qubits {
+			t.Errorf("The number of qubits is %d instead of %d", input_qudit.N_qubits, test_case.ExpectedInputQudit.N_qubits)
+		}
+		for input_state, input_amplitude := range input_qudit.Amplitudes {
+			if input_amplitude != test_case.ExpectedInputQudit.Amplitudes[input_state] {
+				t.Errorf("The amplitude for state %s is %g instead of %g", input_state, input_amplitude, test_case.ExpectedInputQudit.Amplitudes[input_state])
+			}
 		}
 	}
 
